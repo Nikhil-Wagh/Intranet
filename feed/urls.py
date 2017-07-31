6529a961acc57ec141004d60a80c99aa58545545
Nikhil-Wagh <nikhilwagh.work@gmail.com>
2017-08-01 03:34:06 +0530

    Template and views updated.

--- feed/urls.py
+++ feed/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'feed'
+
 urlpatterns = [
     # /feed/
     url(r'^$', views.index, name='index'),
 
-    # /feed/34(Project ID)
+    # /feed/34<project_id>
     url(r'^(?P<project_id>[0-9]+)/$', views.detail, name='detail'),
+    
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,33 +1,42 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Project
 
-
-#from django.template import loader
+# from django.template import loader
 
 # Create your views here.
+app_name = 'feed'
+
 
 def index(request):
     all_projects = Project.objects.all()
     context = {'all_projects': all_projects}
     return render(request, 'feed/index.html', context)
-    #template = loader.get_template('feed/index.html')
-    #return HttpResponse(template.render(context, request))
+    # template = loader.get_template('feed/index.html')
+    # return HttpResponse(template.render(context, request))
 
 
     # html = ' '
     # for project in all_projects:
     #     url = '/feed/' + str(project.id) + '/'
     #     html += '<a href="' + url + '">' + project.name + '</a><br>'
-    #return HttpResponse(html)
+    # return HttpResponse(html)
+
 
 def detail(request, project_id):
-    try:
-        project = Project.objects.get(pk = project_id)
-    except Project.DoesNotExist:
-        raise Http404("Project does not exist.")
+    # try:
+    #     project = Project.objects.get(pk = project_id)
+    # except Project.DoesNotExist:
+    #     raise Http404("Project does not exist.")
+    project = get_object_or_404(Project, pk=project_id)
     return render(request, 'feed/detail.html', {'project': project})
+
+    # class feed_sitemap(Sitemap):
+    #     changefreq = "daily"
+    #     priority = 1.0
+    #     lastmod = datetime.datetime.now()
+    #     def items(self):
+    #         return Project.objects.all()
